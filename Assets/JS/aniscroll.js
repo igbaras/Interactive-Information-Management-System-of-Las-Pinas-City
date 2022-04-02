e3dd7eb7bc99768502e19be48ee28d9b9ae5f49c
kiellavapiez <ezekiel.lavapiez@gmail.com>
2022-04-02 23:24:22 +0800

    try

--- Assets/JS/aniscroll.js
+++ Assets/JS/aniscroll.js
@@ -2,7 +2,7 @@ const html = document.documentElement;
 const canvas = document.querySelector('.airpod-scroll');
 const context = canvas.getContext("2d");
 
-const frameCount = 148;
+const frameCount = 77;
 const currentFrame = index => (
   `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${index.toString().padStart(4, '0')}.jpg`
 )
