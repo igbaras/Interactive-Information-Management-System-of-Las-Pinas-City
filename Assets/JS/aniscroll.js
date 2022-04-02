804e2d40b891b8b20c52d4553cc70b7fba36bcba
kiellavapiez <ezekiel.lavapiez@gmail.com>
2022-04-02 23:52:17 +0800

    try

--- Assets/JS/aniscroll.js
+++ Assets/JS/aniscroll.js
@@ -8,7 +8,7 @@ const currentFrame = index => (
 )
 
 const preloadImages = () => {
-  for (let i = 10; i < frameCount; i++) {
+  for (let i = 1; i < frameCount; i++) {
     const img = new Image();
     img.src = currentFrame(i);
   }
