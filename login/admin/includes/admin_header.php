61fc3738f5dee20fac794049ffe847a9f584e00e
kiellavapiez <ezekiel.lavapiez@gmail.com>
2022-11-23 02:36:01 +0800

    Css Fix

--- login/admin/includes/admin_header.php
+++ login/admin/includes/admin_header.php
@@ -28,7 +28,7 @@ Configuration::instance([
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" type="image icon" href="../Assets/images/lplogo.png">
+    <link rel="shortcut icon" type="image icon" href="../lplogo.png">
     <title>Admin | The City of Las Piñas</title>
 
 
