46d706bf30afb2f55e5596f0db922fd86edda90b
Tiansing <christian_igbaras@yahoo.com>
2022-11-25 00:12:16 +0800

    remote fixed

--- includes/db.php
+++ includes/db.php
@@ -1,15 +1,15 @@
 <?php
 // =============FOR LOCAL CONNECTION===========
-$db["db_host"] = "localhost";
-$db["db_user"] = "root";
-$db["db_pass"] = "";
-$db["db_name"] = "iims";
+// $db["db_host"] = "localhost";
+// $db["db_user"] = "root";
+// $db["db_pass"] = "";
+// $db["db_name"] = "iims";
 
 // =============FOR REMOTE CONNECTION===========
-// $db["db_host"] = "azure-iims.mysql.database.azure.com";
-// $db["db_user"] = "iims";
-// $db["db_pass"] = "i;i;m;s;1234";
-// $db["db_name"] = "iims";
+$db["db_host"] = "azure-iims.mysql.database.azure.com";
+$db["db_user"] = "iims";
+$db["db_pass"] = "i;i;m;s;1234";
+$db["db_name"] = "iims";
 
 foreach ($db as $key => $value) {
     define(strtoupper($key), $value);
