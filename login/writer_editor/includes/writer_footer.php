2a990f9a13aa6c2e008d1f7870d148f92c1efbbf
kiellavapiez <ezekiel.lavapiez@gmail.com>
2022-11-24 14:41:56 +0800

    Footer Fix

--- login/writer_editor/includes/writer_footer.php
+++ login/writer_editor/includes/writer_footer.php
@@ -1,9 +1,3 @@
-<footer class="main-footer">
-    <strong>Copyright &copy; 2022 GROUP 4</strong>
-    All rights reserved.
-
-</footer>
-
 <!-- Control Sidebar -->
 <aside class="control-sidebar control-sidebar-dark">
     <!-- Control sidebar content goes here -->
